199355077404f0189bfa1dbd6557672a3ef271ba
Sam Warmuth <sam@samwarmuth.com>
2011-10-12 00:24:22 -0500

    updated port, added sample data.

--- app/routes/home.rb
+++ app/routes/home.rb
@@ -1,6 +1,7 @@
 class Main
   get "/" do
-    haml :index
+    scores = [{:home_team => {:name => "Brookfield Bruisers"}, :away_team => {:name => "Vernon RFC"}, :date => "11/4"}]
+    return scores.to_json
   end
   get "/css/style.css" do
     content_type 'text/css', :charset => 'utf-8'
