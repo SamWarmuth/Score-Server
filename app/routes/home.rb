53d1ff934bbdb61ec2b84cd8bbde76cbbe033f14
Sam Warmuth <sam@samwarmuth.com>
2011-10-12 01:35:46 -0500

    updated json response.

--- app/routes/home.rb
+++ app/routes/home.rb
@@ -1,6 +1,7 @@
 class Main
   get "/" do
-    scores = [{:home_team => {:name => "Brookfield Bruisers"}, :away_team => {:name => "Vernon RFC"}, :date => "11/4"}]
+    scores = [{:home_team => {:name => "Brookfield Bruisers", :score => 14},
+      :away_team => {:name => "Vernon RFC", :score => 0}, :date => "11/4"}]
     return scores.to_json
   end
   get "/css/style.css" do
